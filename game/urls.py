53db40ee3cf2db0fd8b0b037ce54102753f23441
TomFearn <tomfearn03@gmail.com>
2025-01-31 18:06:14 +0000

    fix template issues

--- game/urls.py
+++ game/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ChessView
 
 urlpatterns = [
-    path("", ChessView.as_view, name='home'),
+    path("", ChessView.as_view(), name='home'),
 ]
--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def register_view(request):
     return render(request, "users/login.html")
+
 def register_view(request):
     form = UserCreationForm()
     return(request, "..\login.html", {"form":form})
